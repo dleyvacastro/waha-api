14826a4f22b68b9f25cdbbe3cbf170dceeafb285
dleyvacastro <dleyvacastro@gmail.com>
2023-05-05 23:33:19 -0500

    count down improved

--- db_manager.py
+++ db_manager.py
@@ -20,44 +20,27 @@ cases = db.cases
 # post_id = posts.insert_one(case).inserted_id
 # print(post_id)
 
-async def close_case_countdown(case_id):
-    print("COUNTDOWN STARTED")
-    await asyncio.sleep(10)
-    # get the case
-    case = cases.find_one({"_id": case_id})
-    time_difference = datetime.datetime.utcnow() - case["last_update"]
-    if time_difference.total_seconds() > 10:
-        cases.update_one({"_id": case_id}, {"$set": {"active": False}})
-        print("Case closed")
-    else:
-        print("Case still active")
-
-
 async def manage_message(payload):
     # get the last message from the user if it exists
     last_case = cases.find_one({"number": payload["from"]}, sort=[("creation_date", -1)])
     print(last_case)
-    if last_case is None:
-        print("Case doesn't exist")
-        case_id = create_case(payload)
-        await close_case_countdown(case_id)
-        return
-
-    time_difference = datetime.datetime.utcnow() - last_case["creation_date"]
-    print(time_difference.total_seconds())
-    if time_difference.total_seconds() > 10 or last_case["active"] is False:
-        create_case(payload)
-        return
+    if last_case is not None:
+        if last_case["active"] is True:
+            print("Case already exists")
+        else:
+            print("Case doesn't exist")
+            await create_case(payload)
+            return
     else:
-        print("Case already exists")
+        print("Case doesn't exist")
+        await create_case(payload)
+        return
 
     # update last update date
     cases.update_one({"_id": last_case["_id"]}, {"$set": {"last_update": datetime.datetime.utcnow()}})
-    # start countdown
-    await close_case_countdown(last_case["_id"])
 
 
-def create_case(payload):
+async def create_case(payload):
     case = {
         "name": payload["_data"]["notifyName"],
         "number": payload["from"],
@@ -67,6 +50,17 @@ def create_case(payload):
         "active": True
     }
     try:
-        return cases.insert_one(case).inserted_id
+        cases.insert_one(case)
+        # close the case in 10 seconds
+        await asyncio.create_task(close_case(case["_id"]))
+
     except errors.DuplicateKeyError:
         print("Case already exists")
+
+
+async def close_case(case_id):
+    while cases.find_one({"_id": case_id})["last_update"] + datetime.timedelta(seconds=10) > datetime.datetime.utcnow():
+        await asyncio.sleep(9)
+
+    cases.update_one({"_id": case_id}, {"$set": {"active": False}})
+    print("Case closed")
